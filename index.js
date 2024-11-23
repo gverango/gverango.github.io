a33c63637c0fddf6cea2c10123398ed21928d567
Gevilee <157779885+gverango@users.noreply.github.com>
2024-11-23 19:56:21 +0000

    OMG javascript.

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ navLinks.forEach(link => {
     this.classList.add('active');
   });
 });
+
+// THEME SWITCH FUNCTIONALITY
+
+let darkmode = localStorage.getItem('darkmode')
+const themeSwitch = document.getElementById('theme-switch')
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode', 'active')
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode',null)
+}
+
+if (darkmode==="active") enableDarkmode()
+
+themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode(): disableDarkmode()
+})
